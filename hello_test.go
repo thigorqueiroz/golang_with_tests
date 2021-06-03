1818bac896b6dfc7a00d828ca2d50832acf36fcb
queiroz <thigorqueiroz@gmail.com>
2021-06-03 16:28:34 -0300

    refactoring tests

--- hello_test.go
+++ hello_test.go
@@ -1,38 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Thiago")
 
-	want := "Hello, Thiago."
+	t.Run("Test Success case", func(t *testing.T) {
+		got := Hello("Thiago")
+		want := "Hello, Thiago."
 
-	if got != want {
-		t.Errorf("Got: %q is not equal Want:  %q", got, want)
-	}
+		if got != want {
+			t.Errorf("Got: %q is not equal Want:  %q", got, want)
+		}
+	})
 
-	fmt.Printf("Got: %q is EQUAL want: %q ", got, want)
-}
+	t.Run("Test With Empty State", func(t *testing.T) {
+		got := Hello("  ")
+		want := "Hello World"
 
-func TestHelloWithEmptyState(t *testing.T) {
-	got := Hello("  ")
-	want := "Hello World"
+		if got != want {
+			t.Errorf("'Got' %q is not equal 'Want' %q ", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("'Got' %q is not equal 'Want' %q ", got, want)
-	}
+	t.Run("Test With Blank", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
 
-	fmt.Println("'Got' is equal 'Want'")
-}
-
-func TestHelloWithBlank(t *testing.T) {
-	got := Hello("")
-	want := "Hello World"
-
-	if got != want {
-		t.Error("There is a diference between GOT and WANT")
-	}
+		if got != want {
+			t.Error("There is a diference between GOT and WANT")
+		}
+	})
 }
