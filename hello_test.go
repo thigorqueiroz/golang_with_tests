fd9aabc713b3fc50b93cb69ebf00db4d8245f7ec
queiroz <thigorqueiroz@gmail.com>
2021-06-11 10:03:00 -0300

    benchmark test

--- hello_test.go
+++ hello_test.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 )
+
+func BenchmarkRandomInt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rand.Int()
+	}
+}
 
 func TestHello(t *testing.T) {
 
 	t.Run("Test Success case", func(t *testing.T) {
-		got := Hello("Thiago")
-		want := "Hello, Thiago."
-
-		if got != want {
-			t.Errorf("Got: %q is not equal Want:  %q", got, want)
-		}
+		fmt.Println(Hello("Thiago"))
+		// Output: Hello, Thiago.
 	})
 
 	t.Run("Test With Empty State", func(t *testing.T) {
