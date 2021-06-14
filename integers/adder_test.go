05e818f9b496e70d4acce1a960b6b89697d801bd
queiroz <thigorqueiroz@gmail.com>
2021-06-14 12:34:46 -0300

    WIP

---
+++ integers/adder_test.go
@@ -0,0 +1,12 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 2)
+	expected := 4
+
+	if got != expected {
+		t.Errorf("'GOT' is diferent from 'EXPECTED'. got : %d | expected : %d", got, expected)
+	}
+}
