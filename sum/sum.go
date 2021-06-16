edb3311b382c65a0248afec59010b5e3049fea20
queiroz <thigorqueiroz@gmail.com>
2021-06-16 08:50:13 -0300

    WIP

--- sum/sum.go
+++ sum/sum.go
@@ -1,9 +1,23 @@
 package sum
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	var total int = 0
 	for _, number := range numbers {
 		total += number
 	}
 	return total
+}
+
+func SumAll(numbers ...[]int) (sum []int) {
+	for index, arr := range numbers {
+		var sumOfThis int = 0
+		for _, number := range arr {
+			sumOfThis += number
+		}
+		fmt.Printf("index: %d", index)
+		//sumOfThis = 0
+	}
+	return
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -11,6 +14,17 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("GOT %d is diferent from WANT %d, value: %v", got, want, numbers)
+		}
+	})
+
+	t.Run("sum of all numbers", func(t *testing.T) {
+		numbersA := []int{1, 2, 3}
+		numbersB := []int{2, 3, 4}
+		got := SumAll(numbersA, numbersB)
+		want := []int{6, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("GOT %d is diferent from WANT %d, values: %v, %v", got, want, numbersA, numbersB)
 		}
 	})
 
