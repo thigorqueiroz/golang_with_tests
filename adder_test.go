ee69dd164eb806ddb5d6471f7e5211d69e94e944
thigorqueiroz <thigorqueiroz@gmail.com>
2021-06-28 05:45:53 -0300

    WIP

--- adder_test.go
+++ adder_test.go
@@ -5,7 +5,6 @@ import "testing"
 func TestAdder(t *testing.T) {
 	got := Add(2, 2)
 	expected := 4
-
 	if got != expected {
 		t.Errorf("'GOT' is diferent from 'EXPECTED'. got : %q | expected : %q")
 	}
