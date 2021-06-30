63d1540d1e76e2e221abc13e4ae27136791c68de
thigorqueiroz <thigorqueiroz@gmail.com>
2021-06-30 06:59:40 -0300

    WIP

--- strut_meth_interface/perimeter_test.go
+++ strut_meth_interface/perimeter_test.go
@@ -23,20 +23,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTest := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{width: 12, heigth: 6}, want: 72.0},
-		{shape: Circle{radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{base: 12, height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{width: 12, heigth: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{base: 12, height: 6}, want: 31.0},
 	}
 
 	for _, tt := range areaTest {
-		got := tt.shape.Area()
 
-		if got != tt.want {
-			t.Errorf("got: %g | want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got: %g | want %g", tt.shape, got, tt.want)
+			}
+		})
 	}
 
 }
