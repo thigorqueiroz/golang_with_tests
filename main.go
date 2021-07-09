1ec9ad578f9627ee3625c0e2d26434db699af36a
thigorqueiroz <thigorqueiroz@gmail.com>
2021-07-09 14:46:34 -0300

    WIP

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
-func greet(w io.Writer, name string) {
+func Greet(w io.Writer, name string) {
 	fmt.Fprintf(w, "Hello, %s", name)
 }
 
 func GreetHandler(w http.ResponseWriter, r *http.Request) {
-	greet(w, "Queiroz")
+	Greet(w, "Queiroz")
 }
 
 func main() {
-	log.Fatal(http.ListenAndServe(":5000", http.HandleFunc(GreetHandler)))
+	http.ListenAndServe(":5000", http.HandlerFunc(GreetHandler))
 }
